917bee63ca6a7f8458bce065348122bc7b635ba9
Projecthing <admirethemakeroftime@gmail.com>
2021-09-12 16:11:30 +0300

    Create basic dispatcher/camera code

--- Dispatcher.cs
+++ Dispatcher.cs
@@ -4,30 +4,52 @@ using UnityEngine;
 
 public class Dispatcher : MonoBehaviour
 {
-    public ComputeShader gol;
-    public RenderTexture board;
-    private RenderTexture previousBoard;
+    private int NUMTHREADS_X = 8;   //No changy this...
+    private int NUMTHREADS_Y = 8;   //nor this...           (Unless you know what you are doing of course)
 
-    public int BoardWidth = 512;
-    public int BoardHeight = 512;
+    public ComputeShader GGol;
+    public RenderTexture Board;
+    private RenderTexture PreviousBoard;
 
-    public bool EnableSim = false;
+    public int boardWidth = 512;
+    public int boardHeight = 512;
+
+    public bool enableSim = false;
+
+    //0=transition to dead, 1=transition to alive if dead, 2=survive if alive, 3=transition to alive if dead and survive if alive
+    public int[] rules = {0, 0, 0, 0, 0, 0, 0, 0};
+
+    private int kernelID;
 
     void Start()
     {
-        if(board == null){
-            
-        }
+        kernelID = GGol.FindKernel("GGOL");
+        CreateTextures();
+        GGol.SetTexture(kernelID, "result", Board);
+        GGol.SetTexture(kernelID, "previous", PreviousBoard);
+        UpdateShader();
     }
 
-    private void OnRenderImage(){
-
+    void Update(){
+        Graphics.CopyTexture(Board, PreviousBoard);
+        GGol.Dispatch(kernelID, boardWidth / NUMTHREADS_X, boardHeight / NUMTHREADS_Y, 1);
     }
 
-    private void createTextures(){
-        board = new RenderTexture(BoardWidth, BoardHeight, 24);
-        previousBoard = new RenderTexture(BoardWidth, BoardHeight, 24);
-        board.enableRandomWrite = true;
-        board.Create();
+    private void OnRenderImage(RenderTexture src, RenderTexture dest){
+        Graphics.Blit(Board, dest);
+    }
+
+    private void CreateTextures(){
+        Board = new RenderTexture(boardWidth, boardHeight, 24);
+        PreviousBoard = new RenderTexture(boardWidth, boardHeight, 24);
+        Board.enableRandomWrite = true;
+        Board.Create();
+        PreviousBoard.Create();
+    }
+
+    private void UpdateShader(){
+        GGol.SetFloat("width", (float)boardWidth);
+        GGol.SetFloat("height", (float)boardHeight);
+        GGol.SetInts("rules", rules);
     }
 }

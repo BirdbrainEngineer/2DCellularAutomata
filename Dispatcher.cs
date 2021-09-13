7b1948243d2be7d51aac563ef28b10da6179e896
Projecthing <admirethemakeroftime@gmail.com>
2021-09-13 18:55:14 +0300

    Dynamic viewport capabilities

--- Dispatcher.cs
+++ Dispatcher.cs
@@ -8,13 +8,17 @@ public class Dispatcher : MonoBehaviour
     private int NUMTHREADS_Y = 8;   //nor this...           (Unless you know what you are doing of course)
 
     public ComputeShader GGol;
+    public ComputeShader Viewport;
     public Texture2D input;
     private RenderTexture board0;
     private RenderTexture board1;
+    private RenderTexture output;
 
     public int boardWidth = 512;
     public int boardHeight = 512;
     public float simSpeed = 1.0f;
+    public float zoom = 100.0f;
+    public float[] viewportCoords = new float[2]{0.0f, 0.0f};
 
     public bool enableSim = false;
 
@@ -35,19 +39,20 @@ public class Dispatcher : MonoBehaviour
     void Start()
     {
         kernelGGOL = GGol.FindKernel("GGOL");
+        kernelViewport = Viewport.FindKernel("Viewport");
         CreateTextures();
         if(input != null && input.width == board0.width && input.height == board0.height) { 
             Graphics.Blit(input, board0);
             Graphics.Blit(input, board1);
         } else { print("Could not use input image - dimensions differ"); }
         StartGGOL();
-        StartViewport();
         nextUpdate = Time.time + simSpeed;
     }
 
     void Update(){
         if(enableSim && nextUpdate <= Time.time) {
             nextUpdate = Time.time + simSpeed;
+            GGol.SetInts("rules", PadRules(rules));
             if(boardState){
                 GGol.SetTexture(kernelGGOL, "result", board0);
                 GGol.SetTexture(kernelGGOL, "input", board1);
@@ -63,8 +68,18 @@ public class Dispatcher : MonoBehaviour
     }
 
     private void OnRenderImage(RenderTexture src, RenderTexture dest){
-        if(boardState){ Graphics.Blit(board1, dest); }
-        else { Graphics.Blit(board0, dest); }
+        output = new RenderTexture(Screen.width, Screen.height, 24);
+        output.enableRandomWrite = true;
+        output.useMipMap = false;
+        output.Create();
+        Viewport.SetTexture(kernelViewport, "output", output);
+        Viewport.SetFloats("viewportCoords", viewportCoords);
+        Viewport.SetFloat("deltaPixel", (zoom / (float)boardWidth) / (float)output.width);
+        if(boardState){ Viewport.SetTexture(kernelViewport, "board", board1); }
+        else { Viewport.SetTexture(kernelViewport, "board", board0); }
+        Viewport.Dispatch(kernelViewport, output.width / NUMTHREADS_X, output.height / NUMTHREADS_Y, 1);
+        Graphics.Blit(output, dest);
+        output.Release();
     }
 
     private void CreateTextures(){
@@ -85,21 +100,20 @@ public class Dispatcher : MonoBehaviour
     private void StartGGOL(){
         float[] deltaPixel = {1.0f / (float)boardWidth, 1.0f / (float)boardHeight, 0.0f, 0.0f};
         GGol.SetFloats("deltaPixel", deltaPixel);
-        int[] paddedArray = new int[rules.Length * 4];  //making the rules array compatible with how the shader accepts data
-        for(int i = rules.Length - 1; i >= 0; i--){ 
+        GGol.SetFloat("width", (float)boardWidth);
+        GGol.SetFloat("height", (float)boardHeight);
+    }
+
+    private int[] PadRules(int[] rulesIn){
+        int[] paddedArray = new int[rulesIn.Length * 4];
+        for(int i = rulesIn.Length - 1; i >= 0; i--){ 
             int paddedIndex = i * 4;
             for(int j = 0; j < 3; j++){
                 paddedArray[paddedIndex + j] = 0;
             }
             paddedArray[paddedIndex + 3] = rules[i];
         }
-        GGol.SetInts("rules", paddedArray);
-        GGol.SetFloat("width", (float)boardWidth);
-        GGol.SetFloat("height", (float)boardHeight);
-    }
-
-    private void StartViewport(){
-
+        return paddedArray;
     }
 }
 
